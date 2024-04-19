b860aad1a24252d035264f8295d84cb45b9c42bc
Suren <48720978+surhak-wmg@users.noreply.github.com>
2024-04-19 12:09:43 +0400

    - minor changes

--- src/SlopesTable.tsx
+++ src/SlopesTable.tsx
@@ -1,7 +1,53 @@
 import React from 'react';
+import Table from 'antd/es/table';
+
+const dataSource = [
+  {
+    key: '1',
+    track: 'Lost in the Fire',
+    chart: 'Top 200',
+    total: '1,000',
+    spotify: '500',
+    appleMusic: '300',
+    pandora: '200',
+  },
+];
+
+const columns = [
+  {
+    title: 'Track',
+    dataIndex: 'track',
+    key: 'track',
+  },
+  {
+    title: 'Chart',
+    dataIndex: 'chart',
+    key: 'chart',
+  },
+  {
+    title: 'Total',
+    dataIndex: 'total',
+    key: 'total',
+  },
+  {
+    title: 'Spotify',
+    dataIndex: 'spotify',
+    key: 'spotify',
+  },
+  {
+    title: 'Apple Music',
+    dataIndex: 'appleMusic',
+    key: 'appleMusic',
+  },
+  {
+    title: 'Pandora',
+    dataIndex: 'pandora',
+    key: 'pandora',
+  },
+];
 
 function SlopesTable() {
-  return <div className="App">Hi</div>;
+  return <Table dataSource={dataSource} columns={columns} />;
 }
 
 export default SlopesTable;
